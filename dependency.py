40014d7bc5aaf4baf5220a5404e0f86aae5bb806
xieqiupeng <xieqiupeng@gmail.com>
2019-09-17 18:04:58 +0800

    update

---
+++ dependency.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Dependency
+ln -sf /Volumes/Project/Enum/Enum-Lesson/嫁出去  $Path/1/嫁出去
+ln -sf /Volumes/Project/Enum/Enum-Lesson/找工作  $Path/5/找工作
+
+# Root
+Path=/Volumes/Project/Classify/ZSJSLL
+ln -sf /Volumes/Project/Enum/Enum-Lesson/嫁出去  $Path/1/嫁出去
