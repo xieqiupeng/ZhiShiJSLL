6031421a4311fef7978ce63dc3d93ebcd9f40e43
xieqiupeng <xieqiupeng@gmail.com>
2019-10-22 11:45:36 +0800

    update

--- dependency.py
+++ dependency.py
@@ -3,8 +3,10 @@
 
 # Dependency
 ln -sf /Volumes/Project/Enum/Enum-Lesson/嫁出去  $Path/1/嫁出去
+ln -sf /Volumes/Project/Enum/Enum-Lesson/有价值  $Path/2/有价值
 ln -sf /Volumes/Project/Enum/Enum-Lesson/找工作  $Path/5/找工作
+ln -sf /Volumes/Project/Enum/Enum-Lesson/不纠结  $Path/8/不纠结
 
 # Root
 Path=/Volumes/Project/Classify/ZSJSLL
-ln -sf /Volumes/Project/Enum/Enum-Lesson/嫁出去  $Path/1/嫁出去
+ln -sf /Volumes/Project/Enum/Enum-Lesson/不纠结  $Path/8/不纠结
