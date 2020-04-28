5ca66ed15ea31382eed53ba85d9ae7f9db768e1d
xieqiupeng <xieqiupeng@gmail.com>
2020-04-29 01:20:22 +0800

    update

--- dependency.py
+++ dependency.py
@@ -7,6 +7,8 @@ ln -sf /Volumes/Project/Enum/Enum-Lesson/有价值  $Path/2/有价值
 ln -sf /Volumes/Project/Enum/Enum-Lesson/找工作  $Path/5/找工作
 ln -sf /Volumes/Project/Enum/Enum-Lesson/不纠结  $Path/8/不纠结
 
+ln -sf /Volumes/Project/Enum/Enum-Lesson/回归不变  $Path/12/最重要的三点/1/回归不变
+
 # Root
-Path=/Volumes/Project/Classify/ZSJSLL
-ln -sf /Volumes/Project/Enum/Enum-Lesson/不纠结  $Path/8/不纠结
+Path=/Volumes/Project/Enum/Enum-Subject/知识就是力量
+ln -sf /Volumes/Project/Enum/Enum-Lesson/回归不变  $Path/12/最重要的三点/1/回归不变
